384d66d05fa1c0beec16bf05a7d87fd6acd39c97
mj <ui2939@gmail.com>
2024-08-12 22:34:28 +0900

    study ts basic

---
+++ src/basic.ts
@@ -0,0 +1,12 @@
+
+let age:number = 30;
+let nondisclosure:boolean = true;
+let arr:number[] = [1,2,3];
+let arr2:Array<number> = [1,2,3];
+let arr3:Array<string> = ["일","이","삼"];
+
+let bTuple:[string, number];
+bTuple = ['z', 1];
+
+bTuple[0].toLowerCase();
+// bTuple[1].toLowerCase(); 타입에 따라 사용가능, 불가능 에러 채킹해줌
